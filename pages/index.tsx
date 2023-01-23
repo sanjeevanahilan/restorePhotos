17712ed1447f4f5844b201e5c2e1baf7f99cd014
Sanjeevan Ahilan <sanjeevanahilan@gmail.com>
2023-01-23 22:05:36 +0000

    helmholtz

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
           className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
           href="/restore"
         >
-          Restore your photos &rarr;
+          Restore your photos, just like Helmholtz &rarr;
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
